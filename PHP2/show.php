4fd73f3c35f2b158d28b9474702eb4c98958c124
GaguliChamathkara <136616090+GaguliChamathkara@users.noreply.github.com>
2023-07-28 23:39:09 +0530

    Add files via upload

---
+++ PHP2/show.php
@@ -0,0 +1,32 @@
+<html>
+<body>
+   
+    <table border="1">
+    <tr>
+        <td> First Name</td>
+        <td> <?php echo $_POST["firstname"]; ?> </td>
+        </tr>
+    <tr>
+        <td> Last Name</td>
+        <td> <?php echo $lName ?></td></tr>
+     <tr>
+        <td> Username</td>
+        <td> <?php echo $userName ?></td></tr>
+     <tr>
+        <td> Password</td>
+        <td> <?php echo $pwd ?></td></tr>
+     <tr>
+        <td> Gender</td>
+        <td> <?php echo $gender ?></td></tr>
+     <tr>
+        <td> Academic Year</td>
+        <td> <?php echo $academicYear ?></td></tr>
+     <tr>
+        <td> pemailwd</td>
+        <td> <?php echo $email ?></td></tr>
+     <tr>
+        <td> Phone</td>
+        <td> <?php echo $phone ?></td></tr>
+    </table>
+    </body>
+</html>
